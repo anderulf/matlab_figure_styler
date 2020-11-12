78b9a6deb99bf6ae7db9d454b8e82be97c4c810e
anderulf <ulfan@outlook.com>
2020-11-12 19:24:59 +0100

    Added option for saving different places

--- figure_styler.m
+++ figure_styler.m
@@ -2,11 +2,17 @@
 clc
 clear all;
 warning('off') % disabled warning messages from importing .fig created by simulink scope
+store_choices = ['current folder', 'one folder above', 'in prefix folder'];
 
 %% -- Settings -- %%
+% Set index of store_at such that:
+% store_at = 1: store in current folder (same folder as this script)
+% store_at = 2: store in above folder
+% store_at = 3: store in new folder under folder where this script is
+store_at = 2;
 figure_title = 'Rogowski coil and current transformer measurements'; % String for title
 filename = 'rog_vs_ideal'; % filename of inputfile in .fig format
-prefix = 'images/'; % The folder where the figure is stored, and where output is saved
+prefix = 'images/'; % The folder where the figure is stored if store_at=3
 x_label = 'Time [t]';
 y_label = 'Current [A]';
 output_type = '.png';
@@ -14,14 +20,22 @@ label_font_size = 18;
 title_font_size = 24;
 
 %% -- Initial setup -- %
-directory  = cd;
-folders   = strfind(directory,'/');
-directory = append(directory(1:folders(end)-1), '/');
+if strcmp(store_choices(store_at), 'current folder')
+    prefix = '';
+elseif strcmp(store_choices(store_at), 'one folder above')
+    directory = cd;
+    folders = strfind(directory,'/');
+    prefix = append(directory(1:folders(end)-1), '/');
+elseif strcmp(store_choices(store_at), 'in prefix folder')
+    prefix = prefix
+else
+    disp('Store at setting is not correct')
+end
 
 input_type = '.fig';
 
-input_file = append(directory, filename, input_type);
-output_file = append(directory, filename, output_type);
+input_file = append(prefix, filename, input_type);
+output_file = append(prefix, filename, output_type);
 
 % Open filename
 disp(append('Styling figure: ', input_file))
